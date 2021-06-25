071d045f78231e5e80e3e8de15b40df348bcf5d9
Maronato <gustavo@maronato.dev>
2021-06-25 17:04:27 -0300

    updates

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ sleep 5
 
 sudo VERSION=$VERSION bash -c 'VERSION=$VERSION docker stack deploy -c <(VERSION=$VERSION docker-compose config) --with-registry-auth router'
 
-echo "Pausing availability of router nodes..."
-sudo docker node update --availability pause $(sudo docker node ls -q --filter node.label=type=router)
-sleep 5
-
 echo "$VERSION" > version
+
+echo "Pausing availability of router nodes..."
+sleep 10
+sudo docker node update --availability pause $(sudo docker node ls -q --filter node.label=type=router)
