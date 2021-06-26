eb07b3beb2fe6fe96d0c13e195696c93a2ce0600
Maronato <gustavo@maronato.dev>
2021-06-26 16:32:40 -0300

    UI stuff

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@ VERSION=$(cat version | awk 'BEGIN{FS=OFS="."} {$2+=1} 1')
 
 sudo VERSION=$VERSION docker-compose pull
 
-echo "Updating availability of router nodes..."
+echo "========================================"
+echo "Updating availability of router nodes"
+echo "========================================"
 sudo docker node update --availability active $(sudo docker node ls -q --filter node.label=type=router)
 sleep 5
 
@@ -16,6 +18,8 @@ sudo VERSION=$VERSION bash -c 'VERSION=$VERSION docker stack deploy -c <(VERSION
 
 echo "$VERSION" > version
 
-echo "Pausing availability of router nodes..."
+echo "========================================"
+echo "Pausing availability of router nodes"
+echo "========================================"
 sleep 10
 sudo docker node update --availability pause $(sudo docker node ls -q --filter node.label=type=router)
